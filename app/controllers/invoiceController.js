8c857fa5909b7227cb8c95bf48e05810e39f4f99
unknown <ktovar@ucol.mx>
2022-03-21 20:47:36 -0600

    funciones validar

--- app/controllers/invoiceController.js
+++ app/controllers/invoiceController.js
@@ -1,22 +1,22 @@
 let mysql=require('../../db/mysql')
 let invoice=require('../models/invoice')
 module.exports = {
+
    create:(req,res)=>{
+
       console.log(req.body.client_id)
-      mysql.query('select * from client where id = ?',req.body.client_id,(err, rows, fields) => {
-         if(!err)
-           if (rows.length == 1)
-               mysql.query('insert into invoice SET ?',req.body,(err,rows,fields)=>{
-                  if(!err)
-                     res.json(rows)
-                  else
-                     res.json(err)
-               })
-            else 
-               res.json('El cliente no existe')
-         else
-            res.json(err)
-      })
+      let resultC = module.exports._validarCliente(req.body.client_id)
+
+      if (resultC)
+         mysql.query('select * from client where id = ?',req.body.client_id,(err, rows, fields) => {
+            if (!err)
+               res.json(rows)
+            else
+               res.json(err)
+         })
+      else 
+         res.json('El cliente no existe')
+
    },
    list:(req,res)=>{
       mysql.query('select * from invoice',(err,rows,fields)=>{
@@ -32,6 +32,18 @@ module.exports = {
             res.json(rows)
          else
             res.json(err)
+      })
+   },
+   
+   _validarCliente: (client_id) => {
+      mysql.query('select * from client where id =?', client_id, (err,rows,fields) => {
+         if (!err)
+            if (rows.length == 1)
+               return true
+            else
+               return false
+         else
+            return false
       })
    }
 }
